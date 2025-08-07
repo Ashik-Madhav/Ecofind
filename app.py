cd1e62ffeb52104b0f86a8d6976661aad3ce9c5e
Ashik-Madhav <ashik47229@gmail.com>
2025-08-07 15:47:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import gdown
 from PIL import Image
 
 # Constants
-MODEL_URL = "https://drive.google.com/file/d/1XwhGkn_C_AkA8W_pAuPBjpVJQp8ZjRKT/view?usp=drivesdk"
+MODEL_URL = "https://drive.google.com/uc?export=download&id=1XwhGkn_C_AkA8W_pAuPBjpVJQp8ZjRKT"
 MODEL_PATH = "crop_classifier_model.h5"
 CROP_INFO_FILE = "crop_info.json"
 CLASS_NAMES = ['Apple', 'Banana', 'Cotton', 'Grapes', 'Jute', 'Maize',
